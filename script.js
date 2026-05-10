2c4fd3ea441c2ea6df23a60ef7d21a01949674bb
hitesh <bhatnagerhitesh@gmail.com>
2026-05-10 08:10:14 +0530

    adding task

--- script.js
+++ script.js
@@ -1,64 +1,61 @@
 let sliderBtn = document.querySelector(".slide-bar")
 let slider = document.querySelector(".slider")
 let sliderContent = document.querySelector(".content")
-let dark = document.querySelector(".dark")
-let light = document.querySelector(".light")
-let lightCss = document.createElement("link")
-let darkCss = document.createElement("link")
 let body = document.querySelector("body")
+let submitBtn = document.querySelector(".submit")
+let taskList = document.querySelector(".task-list")
+let taskText = document.querySelector("#input")
 
 let sliderState = "close"
 
-sliderBtn.addEventListener("click", (e) => {
-    e.stopPropagation()
-    if (sliderState === "close") {
-        slider.style.width = "20rem"
-        sliderState = "open"
-    } else if (sliderState === "open") {
-        slider.style.width = "0px"
-        sliderState = "close"
-    }
-})
+function gearFunction() {
+    sliderBtn.addEventListener("click", (e) => {
+        e.stopPropagation()
+        if (sliderState === "close") {
+            slider.style.width = "20rem"
+            sliderState = "open"
+        } else if (sliderState === "open") {
+            slider.style.width = "0px"
+            sliderState = "close"
+        }
+    })
 
-body.addEventListener("click", (e)=>{
-    if (sliderState === "open") {
-        slider.style.width = "0px"
-        sliderState = "close"
-    }
-})
+    body.addEventListener("click", (e) => {
+        if (sliderState === "open") {
+            slider.style.width = "0px"
+            sliderState = "close"
+        }
+    })
 
-let mode = "light"
-
-function lightBtn() {
-    darkCss.rel = "stylesheet"
-    darkCss.href = "dark.css"
-    darkCss.id = "dark"
-    document.head.appendChild(darkCss)
-    // lightCss.remove()
-    mode = "dark"
 }
+gearFunction()
 
-function darkBtn() {
-    lightCss.rel = "stylesheet"
-    lightCss.href = "style.css"
-    lightCss.id = "light"
-    document.head.appendChild(lightCss)
-    darkCss.remove()
-    mode = "light"
-}
+const tasks = [
 
+]
 
-dark.addEventListener("click", (e) => {
-    e.stopPropagation(e)
-    if (mode === "light") {
-        lightBtn()
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (taskText.value !== "") {
+        let taskHtml = `<div class="task">
+                    <div class="left">
+                        <input type="checkbox" class="checkbox">
+                        <p class="text">${taskText.value}</p>
+                    </div>
+                    <div class="right">
+                        <div class="delete">
+                            <i class="fa-solid fa-trash"></i>
+                        </div>
+                        <div class="edit">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                        </div>
+                    </div>
+                </div>`
+        taskList.innerHTML += `${taskHtml}`
+        taskText.value = ""
+        console.log("hitesh")
     }
+
 })
 
 
-light.addEventListener("click", (e) => {
-    e.stopPropagation(e)
-    if (mode === "dark") {
-        darkBtn()
-    }
-})
